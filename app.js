d8aa12883aabbbca25dfb36e390ae62fb17ac138
RockyBaura <251drougnov@gmail.com>
2022-11-08 11:31:13 +0600

    hamburger toggle added

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 document.documentElement.style.scrollBehavior = "smooth";  //scroll-behavior: smooth;
+
+//toggle hamburger
+const nav = document.querySelector('nav');
+const navMenu = document.querySelector('.nav__menu');
+
+navMenu.addEventListener('click',()=>{
+    nav.classList.toggle('active');
+})
