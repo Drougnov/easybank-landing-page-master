552139c073682628352380db7110cd5a9029ab0e
RockyBaura <251drougnov@gmail.com>
2022-11-08 14:07:26 +0600

    Change overlay when nav is active

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 document.documentElement.style.scrollBehavior = "smooth";  //scroll-behavior: smooth;
 
 //toggle hamburger
+const body = document.body;
 const nav = document.querySelector('nav');
 const navMenu = document.querySelector('.nav__menu');
+const overlay = document.querySelector('.body__overlay');
 
 navMenu.addEventListener('click',()=>{
     nav.classList.toggle('active');
+
+    if(nav.classList.contains('active')){
+        console.log('true')
+        overlay.style.background = "hsla(233, 26%, 24%,.8)";
+        overlay.style.zIndex = "10";
+    }else{
+        overlay.style.background = "transparent";
+        overlay.style.zIndex = "-1";
+    }
 })
+
+//
